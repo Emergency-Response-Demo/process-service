5875e4ed5bc1ca06a6393c637ace8d1fabf8c905
Bernard Tison <bernard.tison@gmail.com>
2019-03-12 20:48:53 +0100

    minimal implementation

---
+++ src/main/java/com/redhat/cajun/navy/process/ProcessServiceApplication.java
@@ -0,0 +1,12 @@
+package com.redhat.cajun.navy.process;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProcessServiceApplication.class, args);
+    }
+}
