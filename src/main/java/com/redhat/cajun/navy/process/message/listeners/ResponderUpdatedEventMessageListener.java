7a979283a8d51a139d9c77cbaa9c97220c3dfc48
Bernard Tison <bernard.tison@gmail.com>
2019-03-31 09:50:19 +0200

    Listener for ResponderUpdatedEvent message

---
+++ src/main/java/com/redhat/cajun/navy/process/message/listeners/ResponderUpdatedEventMessageListener.java
@@ -0,0 +1,98 @@
+package com.redhat.cajun.navy.process.message.listeners;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.JsonPath;
+import com.redhat.cajun.navy.process.message.model.Message;
+import com.redhat.cajun.navy.process.message.model.ResponderUpdatedEvent;
+import org.jbpm.services.api.ProcessService;
+import org.kie.api.runtime.process.ProcessInstance;
+import org.kie.internal.KieInternalServices;
+import org.kie.internal.process.CorrelationKey;
+import org.kie.internal.process.CorrelationKeyFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionTemplate;
+
+@Component
+public class ResponderUpdatedEventMessageListener {
+
+    private static final Logger log = LoggerFactory.getLogger(ResponderUpdatedEventMessageListener.class);
+
+    private final static String TYPE_RESPONDER_UPDATED_EVENT = "ResponderUpdatedEvent";
+
+    @Autowired
+    private ProcessService processService;
+
+    @Autowired
+    private PlatformTransactionManager transactionManager;
+
+    private CorrelationKeyFactory correlationKeyFactory = KieInternalServices.Factory.get().newCorrelationKeyFactory();
+
+    @KafkaListener(topics = "${listener.destination.responder-updated-event}")
+    public void processMessage(@Payload String messageAsJson, @Header(KafkaHeaders.RECEIVED_MESSAGE_KEY) String key,
+                               @Header(KafkaHeaders.RECEIVED_TOPIC) String topic,
+                               @Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition) {
+
+        if (!accept(messageAsJson)) {
+            return;
+        }
+
+        log.debug("Processing '" + TYPE_RESPONDER_UPDATED_EVENT + "' message for responder '" + key + "' from topic:partition '" + topic + ":" + partition + "'");
+
+        Message<ResponderUpdatedEvent> message;
+        try {
+
+            message = new ObjectMapper().readValue(messageAsJson, new TypeReference<Message<ResponderUpdatedEvent>>() {});
+
+            String incidentId = message.getHeaderValue("incidentId");
+            if (incidentId == null || incidentId.isEmpty()) {
+                log.warn("Message contains no header value for incidentId. Message cannot be processed!");
+                return;
+            }
+
+            CorrelationKey correlationKey = correlationKeyFactory.newCorrelationKey(incidentId);
+
+            Boolean available = "success".equals(message.getBody().getStatus());
+
+            TransactionTemplate template = new TransactionTemplate(transactionManager);
+            template.execute((TransactionStatus s) -> {
+                ProcessInstance instance = processService.getProcessInstance(correlationKey);
+                if (instance == null) {
+                    log.warn("Process instance with correlationKey '" + incidentId + "' not found.");
+                    return null;
+                }
+                processService.signalProcessInstance(instance.getId(), "ResponderAvailableEvent", available);
+                return null;
+            });
+
+        } catch (Exception e) {
+            log.error("Error processing msg " + messageAsJson, e);
+            throw new IllegalStateException(e.getMessage(), e);
+        }
+
+    }
+
+    private boolean accept(String messageAsJson) {
+        try {
+            String messageType = JsonPath.read(messageAsJson, "$.messageType");
+            if (TYPE_RESPONDER_UPDATED_EVENT.equalsIgnoreCase(messageType) ) {
+                return true;
+            } else {
+                log.debug("Message with type '" + messageType + "' is ignored");
+            }
+        } catch (Exception e) {
+            log.warn("Unexpected message without 'messageType' field.");
+        }
+        return false;
+    }
+
+}
---
+++ src/test/java/com/redhat/cajun/navy/process/message/listeners/ResponderUpdatedEventMessageListenerTest.java
@@ -0,0 +1,209 @@
+package com.redhat.cajun.navy.process.message.listeners;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+import static org.springframework.test.util.ReflectionTestUtils.setField;
+
+import org.jbpm.process.instance.ProcessInstance;
+import org.jbpm.services.api.ProcessService;
+import org.junit.Before;
+import org.junit.Test;
+import org.kie.internal.process.CorrelationKey;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+
+public class ResponderUpdatedEventMessageListenerTest {
+
+    @Mock
+    private PlatformTransactionManager ptm;
+
+    @Mock
+    private TransactionStatus transactionStatus;
+
+    @Mock
+    private ProcessService processService;
+
+    @Mock
+    private ProcessInstance processInstance;
+
+    @Captor
+    private ArgumentCaptor<CorrelationKey> correlationKeyCaptor;
+
+    private ResponderUpdatedEventMessageListener messageListener;
+
+    @Before
+    public void init() {
+        initMocks(this);
+        messageListener = new ResponderUpdatedEventMessageListener();
+        setField(messageListener, null, ptm, PlatformTransactionManager.class);
+        setField(messageListener, null, processService, ProcessService.class);
+        when(ptm.getTransaction(any())).thenReturn(transactionStatus);
+        when(processInstance.getId()).thenReturn(100L);
+    }
+
+    @Test
+    public void testProcessMessage() {
+        String json = "{" + "\"messageType\" : \"ResponderUpdatedEvent\"," +
+                "\"id\":\"messageId\"," +
+                "\"invokingService\":\"messageSender\"," +
+                "\"timestamp\":1521148332397," +
+                "\"header\" : {\"incidentId\" : \"incident123\"}," +
+                "\"body\" : {" +
+                "\"status\" : \"success\"," +
+                "\"responder\" : {" +
+                "\"id\" : \"responderId\"," +
+                "\"name\" : \"John Doe\"," +
+                "\"phoneNumber\" : \"111-222-333\"," +
+                "\"latitude\" : 30.12345," +
+                "\"longitude\" : -70.12345," +
+                "\"boatCapacity\" : 2," +
+                "\"medicalKit\" : true," +
+                "\"available\" : false" +
+                "}" + "}" + "}";
+
+        when(processService.getProcessInstance(any(CorrelationKey.class))).thenReturn(processInstance);
+
+        messageListener.processMessage(json, "responderId", "test-topic", 1);
+
+        verify(processService).signalProcessInstance(100L, "ResponderAvailableEvent", true);
+        verify(processService).getProcessInstance(correlationKeyCaptor.capture());
+        CorrelationKey correlationKey = correlationKeyCaptor.getValue();
+        assertThat(correlationKey.getName(), equalTo("incident123"));
+    }
+
+    @Test
+    public void testProcessMessageWhenError() {
+        String json = "{" + "\"messageType\" : \"ResponderUpdatedEvent\"," +
+                "\"id\":\"messageId\"," +
+                "\"invokingService\":\"messageSender\"," +
+                "\"timestamp\":1521148332397," +
+                "\"header\" : {\"incidentId\" : \"incident123\"}," +
+                "\"body\" : {" +
+                "\"status\" : \"error\"," +
+                "\"responder\" : {" +
+                "\"id\" : \"responderId\"," +
+                "\"name\" : \"John Doe\"," +
+                "\"phoneNumber\" : \"111-222-333\"," +
+                "\"latitude\" : 30.12345," +
+                "\"longitude\" : -70.12345," +
+                "\"boatCapacity\" : 2," +
+                "\"medicalKit\" : true," +
+                "\"available\" : false" +
+                "}" + "}" + "}";
+
+        when(processService.getProcessInstance(any(CorrelationKey.class))).thenReturn(processInstance);
+
+        messageListener.processMessage(json, "responderId", "test-topic", 1);
+
+        verify(processService).signalProcessInstance(100L, "ResponderAvailableEvent", false);
+        verify(processService).getProcessInstance(correlationKeyCaptor.capture());
+        CorrelationKey correlationKey = correlationKeyCaptor.getValue();
+        assertThat(correlationKey.getName(), equalTo("incident123"));
+    }
+
+    @Test
+    public void testProcessMessageWhenNoHeader() {
+        String json = "{" + "\"messageType\" : \"ResponderUpdatedEvent\"," +
+                "\"id\":\"messageId\"," +
+                "\"invokingService\":\"messageSender\"," +
+                "\"timestamp\":1521148332397," +
+                "\"header\" : {\"wrongHeader\" : \"incident123\"}," +
+                "\"body\" : {" +
+                "\"status\" : \"error\"," +
+                "\"responder\" : {" +
+                "\"id\" : \"responderId\"," +
+                "\"name\" : \"John Doe\"," +
+                "\"phoneNumber\" : \"111-222-333\"," +
+                "\"latitude\" : 30.12345," +
+                "\"longitude\" : -70.12345," +
+                "\"boatCapacity\" : 2," +
+                "\"medicalKit\" : true," +
+                "\"available\" : false" +
+                "}" + "}" + "}";
+
+        messageListener.processMessage(json, "responderId", "test-topic", 1);
+
+        verify(processService, never()).signalProcessInstance(any(), any(), any());
+        verify(processService, never()).getProcessInstance(any(CorrelationKey.class));
+    }
+
+    @Test
+    public void testProcessMessageProcessNotFound() {
+        String json = "{" + "\"messageType\" : \"ResponderUpdatedEvent\"," +
+                "\"id\":\"messageId\"," +
+                "\"invokingService\":\"messageSender\"," +
+                "\"timestamp\":1521148332397," +
+                "\"header\" : {\"incidentId\" : \"incident123\"}," +
+                "\"body\" : {" +
+                "\"status\" : \"error\"," +
+                "\"responder\" : {" +
+                "\"id\" : \"responderId\"," +
+                "\"name\" : \"John Doe\"," +
+                "\"phoneNumber\" : \"111-222-333\"," +
+                "\"latitude\" : 30.12345," +
+                "\"longitude\" : -70.12345," +
+                "\"boatCapacity\" : 2," +
+                "\"medicalKit\" : true," +
+                "\"available\" : false" +
+                "}" + "}" + "}";
+
+        when(processService.getProcessInstance(any(CorrelationKey.class))).thenReturn(null);
+
+        messageListener.processMessage(json, "responderId", "test-topic", 1);
+
+        verify(processService, never()).signalProcessInstance(any(), any(), any());
+        verify(processService).getProcessInstance(correlationKeyCaptor.capture());
+        CorrelationKey correlationKey = correlationKeyCaptor.getValue();
+        assertThat(correlationKey.getName(), equalTo("incident123"));
+    }
+
+    @Test
+    public void testProcessMessageWrongMessageType() {
+        String json = "{" + "\"messageType\" : \"WrongMessageType\"," +
+                "\"id\":\"messageId\"," +
+                "\"invokingService\":\"messageSender\"," +
+                "\"timestamp\":1521148332397," +
+                "\"header\" : {\"incidentId\" : \"incident123\"}," +
+                "\"body\" : {" +
+                "\"status\" : \"success\"," +
+                "\"responder\" : {" +
+                "\"id\" : \"responderId\"," +
+                "\"name\" : \"John Doe\"," +
+                "\"phoneNumber\" : \"111-222-333\"," +
+                "\"latitude\" : 30.12345," +
+                "\"longitude\" : -70.12345," +
+                "\"boatCapacity\" : 2," +
+                "\"medicalKit\" : true," +
+                "\"available\" : false" +
+                "}" + "}" + "}";
+
+        messageListener.processMessage(json, "responderId", "test-topic", 1);
+
+        verify(processService, never()).signalProcessInstance(any(), any(), any());
+        verify(processService, never()).getProcessInstance(any(CorrelationKey.class));
+    }
+
+    @Test
+    public void testProcessMessageWrongMessageStructure() {
+        String json = "{" + "\"field1\" : \"value1\"," +
+                "\"field2\":\"calue2\"" +
+                "}";
+
+        messageListener.processMessage(json, "responderId", "test-topic", 1);
+
+        verify(processService, never()).signalProcessInstance(any(), any(), any());
+        verify(processService, never()).getProcessInstance(any(CorrelationKey.class));
+    }
+
+
+
+
+}
