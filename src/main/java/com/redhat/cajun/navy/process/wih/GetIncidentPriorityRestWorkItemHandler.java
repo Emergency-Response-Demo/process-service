a9d2bd830738ed516d5f47a200350fb4839e14e7
Bernard Tison <bernard.tison@gmail.com>
2019-04-16 20:02:35 +0200

    WorkItemhandler to call incident priority service

---
+++ src/main/java/com/redhat/cajun/navy/process/wih/GetIncidentPriorityRestWorkItemHandler.java
@@ -0,0 +1,91 @@
+package com.redhat.cajun.navy.process.wih;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.redhat.cajun.navy.rules.model.Incident;
+import com.redhat.cajun.navy.rules.model.IncidentPriority;
+import org.kie.api.runtime.process.WorkItem;
+import org.kie.api.runtime.process.WorkItemHandler;
+import org.kie.api.runtime.process.WorkItemManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+@Component("IncidentPriorityService")
+public class GetIncidentPriorityRestWorkItemHandler implements WorkItemHandler {
+
+    private static Logger log = LoggerFactory.getLogger(GetIncidentPriorityRestWorkItemHandler.class);
+
+    @Value("${incident-priority.service.scheme}")
+    private String serviceScheme;
+
+    @Value("${incident-priority.service.url}")
+    private String serviceUrl;
+
+    @Value("${incident-priority.service.incident-priority-path}")
+    private String incidentPriorityPath;
+
+    @Override
+    public void executeWorkItem(WorkItem workItem, WorkItemManager manager) {
+
+        Object incidentObj = workItem.getParameter("Incident");
+        if (!(incidentObj instanceof Incident)) {
+            throw new IllegalStateException("Parameter 'Incident' cannot be null and must be of type com.redhat.cajun.navy.rules.model.Incident");
+        }
+        Incident incident = (Incident) incidentObj;
+        RestTemplate restTemplate = new RestTemplate();
+        IncidentPriority incidentPriority;
+        try {
+            RestIncidentPriority ip = restTemplate.exchange(serviceScheme + "://" + serviceUrl + incidentPriorityPath,
+                    HttpMethod.GET, null, new ParameterizedTypeReference<RestIncidentPriority>() {}, incident.getId()).getBody();
+            log.debug("Incident Priority for incident '" + ip.incidentId + "': Priority = " + ip.priority + ", Average = " + ip.average);
+            incidentPriority = new IncidentPriority();
+            incidentPriority.setIncidentId(ip.incidentId);
+            incidentPriority.setPriority(new BigDecimal(ip.priority));
+            incidentPriority.setAveragePriority(new BigDecimal(ip.average));
+        } catch (HttpClientErrorException e) {
+            log.error("Http Exception when calling incident priority service - response code : " + e.getRawStatusCode(), e);
+            incidentPriority = new IncidentPriority();
+            incidentPriority.setIncidentId(incident.getId());
+            incidentPriority.setPriority(new BigDecimal(0));
+            incidentPriority.setAveragePriority(new BigDecimal(0));
+        }
+        Map<String, Object> results = new HashMap<>();
+        results.put("IncidentPriority", incidentPriority);
+        manager.completeWorkItem(workItem.getId(), results);
+    }
+
+    @Override
+    public void abortWorkItem(WorkItem workItem, WorkItemManager manager) {
+
+    }
+
+
+    public static class RestIncidentPriority {
+        private String incidentId;
+
+        private int priority;
+
+        private double average;
+
+        public void setIncidentId(String incidentId) {
+            this.incidentId = incidentId;
+        }
+
+        public void setPriority(int priority) {
+            this.priority = priority;
+        }
+
+        public void setAverage(double average) {
+            this.average = average;
+        }
+    }
+
+}
---
+++ src/test/java/com/redhat/cajun/navy/process/wih/GetIncidentPriorityRestWorkItemHandlerTest.java
@@ -0,0 +1,90 @@
+package com.redhat.cajun.navy.process.wih;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.getRequestedFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.verify;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import com.redhat.cajun.navy.rules.model.Incident;
+import com.redhat.cajun.navy.rules.model.IncidentPriority;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.kie.api.runtime.process.WorkItem;
+import org.kie.api.runtime.process.WorkItemManager;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.test.util.ReflectionTestUtils;
+
+public class GetIncidentPriorityRestWorkItemHandlerTest {
+
+    @Rule
+    public WireMockRule wireMockRule = new WireMockRule(wireMockConfig().dynamicPort());
+
+    @Mock
+    private WorkItem workItem;
+
+    @Mock
+    private WorkItemManager workItemManager;
+
+    @Captor
+    private ArgumentCaptor<Map<String, Object>> resultsCaptor;
+
+    private GetIncidentPriorityRestWorkItemHandler wih;
+
+    @Before
+    public void beforeTest() {
+        initMocks(this);
+        wih = new GetIncidentPriorityRestWorkItemHandler();
+        ReflectionTestUtils.setField(wih, "serviceScheme", "http", null);
+        ReflectionTestUtils.setField(wih, "serviceUrl", "localhost:" + wireMockRule.port(), null);
+        ReflectionTestUtils.setField(wih, "incidentPriorityPath", "/priority/{incidentId}", null);
+        when(workItem.getId()).thenReturn(1L);
+    }
+
+
+    @Test
+    public void testWorkItemHandler() throws Exception {
+
+        String ip = "{" + "\"incidentId\": \"incident123\"," + "\"priority\": 1," + "\"average\": 2.5" + "}";
+
+        stubFor(get(urlEqualTo("/priority/incident123")).willReturn(
+                aResponse().withStatus(200).withHeader("Content-type", "application/json")
+                        .withBody(ip)));
+
+        Incident incident = new Incident();
+        incident.setId("incident123");
+
+        when(workItem.getParameter("Incident")).thenReturn(incident);
+
+        wih.executeWorkItem(workItem, workItemManager);
+        verify(getRequestedFor(urlEqualTo("/priority/incident123")));
+        Mockito.verify(workItemManager).completeWorkItem(eq(1L), resultsCaptor.capture());
+        Map<String, Object> results = resultsCaptor.getValue();
+        assertThat(results, notNullValue());
+        assertThat(results.get("IncidentPriority"), notNullValue());
+        assertThat(results.get("IncidentPriority") instanceof IncidentPriority, is(true));
+        IncidentPriority incidentPriority = (IncidentPriority) results.get("IncidentPriority");
+        assertThat(incidentPriority.getIncidentId(), equalTo("incident123"));
+        assertThat(incidentPriority.getPriority(), equalTo(new BigDecimal(1)));
+        assertThat(incidentPriority.getAveragePriority(), equalTo(new BigDecimal(2.5)));
+    }
+
+
+}
