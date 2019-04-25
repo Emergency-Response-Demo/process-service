05c34b10ed222ca7242202b0fafe185ad8385787
Bernard Tison <bernard.tison@gmail.com>
2019-04-25 06:02:17 +0200

    Executor Service Configuration

--- src/main/java/com/redhat/cajun/navy/process/JbpmDeploymentServiceConfiguration.java
+++ src/main/java/com/redhat/cajun/navy/process/JbpmDeploymentServiceConfiguration.java
@@ -8,20 +8,25 @@ import org.jbpm.kie.services.impl.bpmn2.BPMN2DataServiceImpl;
 import org.jbpm.runtime.manager.impl.jpa.EntityManagerFactoryManager;
 import org.jbpm.services.api.DefinitionService;
 import org.jbpm.services.api.DeploymentService;
+import org.kie.api.executor.ExecutorService;
 import org.kie.api.runtime.manager.RuntimeManagerFactory;
 import org.kie.internal.identity.IdentityProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class JbpmConfiguration {
+public class JbpmDeploymentServiceConfiguration {
 
     protected static final String PERSISTENCE_UNIT_NAME = "org.jbpm.domain";
 
     private ApplicationContext applicationContext;
 
-    public JbpmConfiguration(ApplicationContext applicationContext) {
+    @Autowired
+    private ExecutorService executorService;
+
+    public JbpmDeploymentServiceConfiguration(ApplicationContext applicationContext) {
         this.applicationContext = applicationContext;
     }
 
@@ -39,6 +44,9 @@ public class JbpmConfiguration {
         ((SpringKModuleDeploymentService) deploymentService).setContext(applicationContext);
 
         ((SpringKModuleDeploymentService) deploymentService).addListener(((BPMN2DataServiceImpl) definitionService));
+
+        ((SpringKModuleDeploymentService) deploymentService).setExecutorService(executorService);
+        executorService.init();
 
         return deploymentService;
     }
