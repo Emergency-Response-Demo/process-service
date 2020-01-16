3593b1ba6b6df84909eeb1d78b7290421ed4c909
Andy Krohg <akrohg@redhat.com>
2020-01-15 20:44:21 -0500

    Revert "Send coordinates with priority request to immediately detect whether this incident is in a priority zone"
    This reverts commit 378aaf1a9d2ff3c5d8b361fd85b739e4dad064dc.

--- src/test/java/com/redhat/cajun/navy/process/wih/GetIncidentPriorityRestWorkItemHandlerTest.java
+++ src/test/java/com/redhat/cajun/navy/process/wih/GetIncidentPriorityRestWorkItemHandlerTest.java
@@ -1,8 +1,8 @@
 package com.redhat.cajun.navy.process.wih;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
-import static com.github.tomakehurst.wiremock.client.WireMock.post;
-import static com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.getRequestedFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import static com.github.tomakehurst.wiremock.client.WireMock.verify;
@@ -65,20 +65,17 @@ public class GetIncidentPriorityRestWorkItemHandlerTest {
         String ip = "{" + "\"incidentId\": \"incident123\"," + "\"priority\": 1,"
                 + "\"average\": 2.5," + "\"incidents\": 3" +"}";
 
-        stubFor(post(urlEqualTo("/priority/incident123")).willReturn(
+        stubFor(get(urlEqualTo("/priority/incident123")).willReturn(
                 aResponse().withStatus(200).withHeader("Content-type", "application/json")
                         .withBody(ip)));
 
-
         Incident incident = new Incident();
         incident.setId("incident123");
-        incident.setLatitude(BigDecimal.ZERO);
-        incident.setLongitude(BigDecimal.ZERO);
 
         when(workItem.getParameter("Incident")).thenReturn(incident);
 
         wih.executeWorkItem(workItem, workItemManager);
-        verify(postRequestedFor(urlEqualTo("/priority/incident123")));
+        verify(getRequestedFor(urlEqualTo("/priority/incident123")));
         Mockito.verify(workItemManager).completeWorkItem(eq(1L), resultsCaptor.capture());
         Map<String, Object> results = resultsCaptor.getValue();
         assertThat(results, notNullValue());
